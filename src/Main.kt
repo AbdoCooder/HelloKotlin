349cefe341e27103bbde7a180e41315017f824c4
AbdoCooder <abdocooder@gmail.com>
2024-08-11 02:03:18 +0100

    fix

--- src/Main.kt
+++ src/Main.kt
@@ -1,7 +1,11 @@
 fun main() {
     val count = 5
-    val a = add(count,6)
+    val a = add(count,5)
+    val b = sub(count,3)
     println(a)
+    println(b)
+    println(a)
+    println(b)
 }
 
 fun add(a: Int, b: Int): Int = a + b
